71d032541fcf54acb7b952a232152c1c6274f970
wichru <wicher.maciej@gmail.com>
2018-11-22 19:13:25 +0100

    Refactor Rspec to allow create user

--- chimera/spec/models/user_spec.rb
+++ chimera/spec/models/user_spec.rb
@@ -2,23 +2,35 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
+
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_length_of(:first_name).is_at_most(100) }
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_length_of(:last_name).is_at_most(100) }
     it { is_expected.to validate_presence_of(:email) }
 
-    context 'when email has wrong format' do
+    describe 'email field' do
+      subject { User.new(first_name: 'jan', last_name: 'kowalski', email: 'jan@foo.com').valid? }
 
-      it 'complains for invalid format' do
+      context 'when email has wrong format' do
+        let(:email) { 'jan@foo' }
 
+        it 'complains for invalid format' do
+          is_expected.to eq false
+        end
+
+        let(:email) { 'jan' }
+
+        it 'complains for invalid format' do
+          is_expected.to eq false
+        end
       end
-    end
 
-    context 'when email has correct format' do
+      context 'when email has correct format' do
 
-      it 'accepts valid format' do
-
+        it 'accepts valid format' do
+          is_expected.to eq true
+        end
       end
     end
   end
