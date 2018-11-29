2f57ed65be8b059c905061dfa3ba00b46ff4bed3
wichru <wicher.maciej@gmail.com>
2018-11-29 20:49:46 +0100

    Trying to resolve :company_name error

--- chimera_forms/app/models/concerns/user_validations.rb
+++
@@ -1,9 +0,0 @@
-module UserValidations
-  extend ActiveSupport::Concern
-
-  included do
-    validates :first_name, presence: true, length: { maximum: 100 }
-    validates :last_name, presence: true, length: { maximum: 100 }
-    validates :email, presence: true, format: { with: /\A.+@.+\..+\z/ }
-  end
-end
--- chimera_forms/db/migrate/20181123224738_create_companies.rb
+++
@@ -1,9 +0,0 @@
-class CreateCompanies < ActiveRecord::Migration[5.2]
-  def change
-    create_table :companies do |t|
-      t.string :name
-      t.references :user, foreign_key: true
-      t.timestamps
-    end
-  end
-end
